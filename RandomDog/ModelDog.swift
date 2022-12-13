5f0f5e32c0d23cf6301ba2624a4689a1e9aa82ef
Valentin <vaminov94@gmail.com>
2022-12-13 17:31:08 +0300

    model

---
+++ RandomDog/ModelDog.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  RandomDog
+//
+//  Created by Mac on 13.12.2022.
+//
+
+struct Dog: Decodable {
+    let message: String
+    let status: String
+}
--- RandomDog/ViewController.swift
+++ RandomDog/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchImage()
     }
-
-
+    
+    
+    
+    private func fetchImage() {
+        guard let url = URL(string: "https://dog.ceo/api/breeds/image/random") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let dog = try JSONDecoder().decode(Dog.self, from: data)
+                print(dog)
+            } catch let error {
+                print(error)
+            }
+        }.resume()
+    }
 }
 
